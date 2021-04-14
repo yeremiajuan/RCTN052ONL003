212c6d93d7b3f822745da0958a0def1c69346bc8
Yeremia Juan <yeremiajuant@gmail.com>
2021-04-14 14:29:39 +0700

    test

---
+++ src/components/Sidebar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+export default class About extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            judul: "About Me",
+            email: "yeremiajuant@gmail.com",
+            nohp: "08128294xxxx",
+            about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.judul}</h1>
+                <p>{this.state.email}, {this.state.nohp}</p>
+                <p>{this.state.about}</p>
+                
+            </div>
+        );
+    }
+}
